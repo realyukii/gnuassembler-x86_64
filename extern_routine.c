5a246fc654d5e16a9ad98dfe7da0001380dc87a9
Real Yuki <me@reyuki.site>
2025-05-29 23:22:56 +0700

    add datahex routine that will convert an ascii-hex to actual bytes based on these hex

--- extern_routine.c
+++ extern_routine.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 void xorstr(unsigned char *bytes, int len, int randomkey)
 {
@@ -6,3 +8,37 @@ void xorstr(unsigned char *bytes, int len, int randomkey)
 		bytes[i] ^= randomkey;
 }
 
+/* credit: https://stackoverflow.com/a/35452093/22382954 */
+uint8_t *datahex(char *string)
+{
+	size_t slength = strlen(string);
+	if ((slength % 2) != 0) // must be even
+		return NULL;
+
+	size_t dlength = slength / 2;
+
+	uint8_t *data = malloc(dlength);
+	memset(data, 0, dlength);
+
+	size_t index = 0;
+	while (index < slength) {
+		char c = string[index];
+		int value = 0;
+		if (c >= '0' && c <= '9')
+			value = (c - '0');
+		else if (c >= 'A' && c <= 'F')
+			value = (10 + (c - 'A'));
+		else if (c >= 'a' && c <= 'f')
+			value = (10 + (c - 'a'));
+		else {
+			free(data);
+			return NULL;
+		}
+
+		data[(index / 2)] += value << (((index + 1) % 2) * 4);
+
+		index++;
+	}
+
+	return data;
+}
